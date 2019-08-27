ff66795ef3b65f86c585705d611eaf4dbb474fa9
Viktor Hansson <viktor.hansson@dataductus.se>
2019-08-27 14:58:38 +0200

    fixed stuff

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}/../OctoPrint
+
 echo "Workdir: ${DIR}"
-echo "Make backups of the old files..."
-cp ${DIR}/../OctoPrint/src/octoprint/static/css/octoprint.css ${DIR}/../OctoPrint/src/octoprint/static/css/octoprint.css.bak
-cp ${DIR}/../OctoPrint/src/octoprint/templates/index.jinja2 ${DIR}/../OctoPrint/src/octoprint/templates/index.jinja2.bak
 
 echo "Install the modifications..."
 cat ${DIR}/style.css >> ${DIR}/../OctoPrint/src/octoprint/static/css/octoprint.css
 cp ${DIR}/index.jinja2 ${DIR}/../OctoPrint/src/octoprint/templates/index.jinja2
 
+CUR=$(PWD)
+cd ${DIR}/../OctoPrint
+../oprint/bin/pip2 install -e .
+sudo systemctl restart octoprint
+cd ${CUR}
 echo "Finished"
