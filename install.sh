3e7e50873a939a174097ed7f4186bde5ecf4dad1
Viktor Hansson <viktor.hansson@dataductus.se>
2019-08-28 08:53:06 +0200

    fixed issue with pwd

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Install the modifications..."
 cat ${DIR}/style.css >> ${DIR}/../OctoPrint/src/octoprint/static/css/octoprint.css
 cp ${DIR}/index.jinja2 ${DIR}/../OctoPrint/src/octoprint/templates/index.jinja2
 
-CUR=$(PWD)
+CUR=$(pwd)
 cd ${DIR}/../OctoPrint
 ../oprint/bin/pip2 install -e .
 sudo systemctl restart octoprint
